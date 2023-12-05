bcb47aa89c643033545c25c86aa8eefe8fee0eed
codewithjarso <stephenjarso6@gmail.com>
2023-12-05 09:54:12 -0800

    updates

--- _printf.c
+++ _printf.c
@@ -21,10 +21,9 @@ int _printf(const char *format, ...)
 		{
 			switch (*++format)
 			{
-				case 'c': {
-						count += write(1, (char)va_arg(args, int), 1);
-						break;
-					}
+				case 'c':
+					count += write(1, (char)va_arg(args, int), 1);
+					break;
 				case 's': {
 						  const char *str = va_arg(args, const char *);
 
