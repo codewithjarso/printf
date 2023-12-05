7f5578d67f275dea2373446641123f76d82f106e
codewithjarso <stephenjarso6@gmail.com>
2023-12-05 10:09:51 -0800

    second fix

--- _printf.c
+++ _printf.c
@@ -41,7 +41,9 @@ int _printf(const char *format, ...)
 					count += write(1, "%", 1);
 					break;
 				default:
+					count += write(1, format - 1, 1);
 					count += write(1, format, 1);
+					break;
 			}
 		}
 		format++;
