20e251f59949a3021d29f1afbf35399122530d1a
codewithjarso <stephenjarso6@gmail.com>
2023-12-06 14:03:34 -0800

    commit

--- _printf.c
+++ _printf.c
@@ -24,10 +24,12 @@ int _printf(const char *format, ...)
 		{
 			switch (*++format)
 			{
-				case 'c':
-					count += write(1, (char)va_arg(args, int), 1);
+				case 'c': {
+						  char c = (char)va_arg(args, int), 1);
+
+					count += write(1, &c, 1);
 					break;
-				case 's': {
+					  } case 's': {
 						  const char *str = va_arg(args, const char *);
 
 					if (str == NULL)
