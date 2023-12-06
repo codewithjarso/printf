4c1fa9cd9c05245c65f94844885111d6678245bc
codewithjarso <stephenjarso6@gmail.com>
2023-12-05 23:25:53 -0800

    third commit

--- _printf.c
+++ _printf.c
@@ -13,8 +13,8 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (*format == '\0')
-		return (-1);
+	if (format == NULL)
+		return (0);
 	while (*format != '\0')
 	{
 		if (*format != '%')
